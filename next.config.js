3390174c39e87e942662506dea09a5cf4dab358d
Harshit Rathi <harshitr2001@gmail.com>
2022-04-23 11:55:36 +0530

    updates

---
+++ next.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        DB_LOCAL_URI: '',
+        DB_URI: 'mongodb+srv://harshit:harshit@cluster0.yosph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
+        STRIPE_API_KEY: 'pk_test_51JkRMBSBeKl8WkWItc8FO9xH1JR2dr1OirUaA8ddTg8BQGOsJoTs2Ahmw6byL88bvCbhr8rbbXIeEaZCzYXFhc0H00voNs1HVP',
+        STRIPE_SECRET_KEY: 'sk_test_51JkRMBSBeKl8WkWIrw6Wotx7pUEzTwK8xnWMvMNDUYFlFuZLko8yq8YO4RnSndU6bnj1e4CuuFVS0HBtLfwLNAz600vASp5kgi',
+
+        STRIPE_WEBHOOK_SECRET: 'whsec_69c7a67725c7981a4117268d56ea9ad5e29ccf12882b820799ba8df85d58ca9d',
+        CLOUDINARY_CLOUD_NAME: 'harshit111',
+        CLOUDINARY_API_KEY: '872437891895558',
+        CLOUDINARY_API_SECRET: 'N9l0hKaYJECvTShq2YejDvAmsEM',
+
+        SMTP_HOST: "",
+        SMTP_PORT: "",
+        SMTP_USER: "harshitrathi200105@gmail.com",
+        SMTP_PASSWORD: "kwjoyehfmxzzzugo",
+        SMTP_FROM_EMAIL: "harshitrathi200105@gmail.com",
+        SMTP_FROM_NAME: "Harshit Rathi",
+        NEXTAUTH_URL: "",
+    },
+    images: {
+        domains: ['res.cloudinary.com'],
+    },
+}
